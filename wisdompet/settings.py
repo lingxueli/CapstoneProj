b14d641dbdf3bb55e92ece75dbfeb4ece2634cc9
Ubuntu <ubuntu@ip-172-31-9-106.us-west-2.compute.internal>
2020-08-27 17:55:05 +0000

    turn off debug mode for djangow

--- wisdompet/settings.py
+++ wisdompet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fz9_(i9lfytz_c9=izv*7idhia*-c4p1#wc_23d^e+tpcxq%f3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
